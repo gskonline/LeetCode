af483d91952823634ce879073bff18908efd80f9
Sathish Kumar Gugendaren <gskonline@outlook.com>
2017-02-06 21:56:25 -0800

    Solved Keyboard row.

---
+++ KeyboardRow.cs
@@ -0,0 +1,53 @@
+public class Solution {
+     public string[] FindWords(string[] words)
+        {
+
+            var wordsList = words.ToList();
+            int index = 0;
+
+            while ( index < wordsList.Count)
+            {
+                if (CheckWord(wordsList[index]))
+                {
+                    wordsList.RemoveAt(index);
+                }
+                else
+                {
+                    index++;
+                }
+            }
+            
+            //for (int i = 0; i < wordsList.Count - 1; i++)
+            //{
+            //    if (CheckWord(wordsList[i])) wordsList.RemoveAt(i);
+
+            //}
+            return wordsList.ToArray();
+
+        }
+
+        private bool CheckWord(string word)
+        {
+            word = word.ToUpper();
+            List<string> strList = new List<string>() { "QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM" };
+            var wordArray = word.ToCharArray().Distinct().ToArray();
+
+            int j = 0;
+            int homeIndex = -1;
+
+            do
+            {
+                if (strList[j].Contains(wordArray[0])) homeIndex = j;
+                j++;
+            } while (homeIndex < 0);
+
+            for (int i = 0; i <= wordArray.Length - 1; i++)
+            {
+                if (!strList[homeIndex].Contains(wordArray[i])) return true;
+
+            }
+
+
+            return false;
+        }
+}
